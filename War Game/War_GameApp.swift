9075b26624e20a3bbf021604b3115aabb613af6f
amirintech <amir224030@bue.edu.eg>
2023-06-03 13:16:43 +0300

    Initial Commit

---
+++ War Game/War_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_GameApp.swift
+//  War Game
+//
+//  Created by Amir on 03/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct War_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
